637689d39e5949170a6a1efcf06593856e0b3a03
aleajane <heyya.its.alea@gmail.com>
2018-07-31 19:01:32 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -4,7 +4,16 @@ Bundler.require
 class MyApp < Sinatra::Base
 
   get '/' do
+    @name = "Alea"
     erb :index
+  end
+  
+    get '/potato' do
+    erb :potato
+  end
+  
+   get '/berry' do
+    erb :berry
   end
 
 end
